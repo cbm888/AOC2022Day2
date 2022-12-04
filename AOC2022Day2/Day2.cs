f9c5fa9361d7edf3fc0e33ed9c27311e0074b606
Clint <cbmarch88@gmail.com>
2022-12-03 19:46:52 -0600

    Add project files.

---
+++ AOC2022Day2/Day2.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AOC2022Day2
+{
+    public class Day2
+    {
+        string[] day2Data = File.ReadAllLines("day2.txt");
+
+        public void Solutions()
+        {
+            int score = 0;
+            int score2 = 0;
+
+            foreach (string s in day2Data)
+            {
+                string[] moves = s.Split(' ');
+
+                if (moves[0] == "A")
+                {
+                    switch (moves[1])
+                    {
+                        case "X":
+                            score += (1 + 3);
+                            score2 += 3;
+                            break;
+                        case "Y":
+                            score += (2 + 6);
+                            score2 += (1 + 3);
+                            break;
+                        case "Z":
+                            score += 3;
+                            score2 += (2 + 6);
+                            break;
+                    }
+                }
+                if (moves[0] == "B")
+                {
+                    switch (moves[1])
+                    {
+                        case "Y":
+                            score += (2 + 3);
+                            score2 += (2 + 3);
+                            break;
+                        case "Z":
+                            score += (3 + 6);
+                            score2 += (3 + 6);
+                            break;
+                        case "X":
+                            score += 1;
+                            score2 += 1;
+                            break;
+                    }
+                }
+                if (moves[0] == "C")
+                {
+                    switch (moves[1])
+                    {
+                        case "Z":
+                            score += (3 + 3);
+                            score2 += (1 + 6);
+                            break;
+                        case "X":
+                            score += (1 + 6);
+                            score2 += 2;
+                            break;
+                        case "Y":
+                            score += 2;
+                            score2 += (3 + 3);
+                            break;
+                    }
+                }
+
+            }
+
+            Console.WriteLine("Part 1: {0}", score);
+            Console.WriteLine("Part 2: {0}", score2);
+        }
+    }
+}
---
+++ AOC2022Day2/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using AOC2022Day2;
+
+Console.WriteLine("Hello, World!");
+Day2 day2 = new Day2();
+day2.Solutions();
